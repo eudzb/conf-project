dd085f55597674b5797b19e51722a145c6165a97
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2019-06-26 15:32:23 +0200

    search bar

--- src/index.php
+++ src/index.php
@@ -19,10 +19,13 @@
           <h3>Les conférences</h3>
           <div class="row">
             <a href="page-conf.php" class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
-              <img
-              src="../img/dann.png"
-              alt="img-conf"
-              class="img-conf-profil mb-3">
+              <div class="position-relative w-100 mb-3">
+                <img
+                src="../img/dann.png"
+                alt="img-conf"
+                class="img-conf-profil">
+                <span class="conf-categ bg-danger text-light p-1">[Technologie]</span>
+              </div>
               <span class="text-info font-weight-light ml-3">mer. 21 nov, 9h45</span>
               <h5 class="p-3 mb-0">Nouveautés Angular 8+</h5>
               <span class="d-block pl-3 pr-3 mb-2">
@@ -36,17 +39,24 @@
                  <i class="p48 text-muted material-icons">location_on</i>
                  <span class="font-weight-light align-self-center">21 Rue Claude Tillier - 75012 Paris</span>
                </div>
+              <div class="float-left pl-3 pt-2">
+                <i class="text-muted material-icons dp48">person</i>
+                <span class="vertical-super">102</span>
+              </div>
               <button
               type="button"
               name="participer"
-              class="btn btn-outline-primary mb-3 mr-3 float-right">Participer</button>
+              class="btn btn-outline-primary mb-3 mr-3 float-right">Détails</button>
               </a>
 
-            <div class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
-              <img
-              src="../img/hiruma.jpeg"
-              alt="img-conf"
-              class="img-conf-profil mb-3">
+            <a href="page-conf.php" class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
+              <div class="position-relative w-100 mb-3 mb-3">
+                <img
+                src="../img/hiruma.jpeg"
+                alt="img-conf"
+                class="img-conf-profil">
+                <span class="conf-categ bg-success text-light p-1">[Famille]</span>
+              </div>
               <span class="text-info font-weight-light ml-3">mer. 21 nov, 9h45</span>
               <h5 class="p-3 mb-0">Nouveautés Angular 8+</h5>
               <span class="d-block pl-3 pr-3 mb-2">
@@ -60,17 +70,24 @@
                  <i class="p48 text-muted material-icons">location_on</i>
                  <span class="font-weight-light align-self-center">21 Rue Claude Tillier - 75012 Paris</span>
                </div>
-              <button
-              type="button"
-              name="participer"
-              class="btn btn-outline-primary mb-3 mr-3 float-right">Participer</button>
-            </div>
+              <div class="float-left pl-3 pt-2">
+                <i class="text-muted material-icons dp48">person</i>
+                <span class="vertical-super">59</span>
+              </div>
+               <button
+                type="button"
+                name="participer"
+                class="btn btn-outline-primary mb-3 mr-3 float-right">Détails</button>
+            </a>
 
-            <div class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
-              <img
-              src="../img/meme.png"
-              alt="img-conf"
-              class="img-conf-profil mb-3">
+            <a href="page-conf.php" class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
+              <div class="position-relative w-100 mb-3">
+                <img
+                src="../img/meme.png"
+                alt="img-conf"
+                class="img-conf-profil">
+                <span class="conf-categ bg-warning text-light p-1">[Nature & Aventure]</span>
+              </div>
               <span class="text-info font-weight-light ml-3">mer. 21 nov, 9h45</span>
               <h5 class="p-3 mb-0">Nouveautés Angular 8+</h5>
               <span class="d-block pl-3 pr-3 mb-2">
@@ -84,17 +101,24 @@
                  <i class="p48 text-muted material-icons">location_on</i>
                  <span class="font-weight-light align-self-center">21 Rue Claude Tillier - 75012 Paris</span>
                </div>
+              <div class="float-left pl-3 pt-2">
+                <i class="text-muted material-icons dp48">person</i>
+                <span class="vertical-super">319</span>
+              </div>
               <button
               type="button"
               name="participer"
-              class="btn btn-outline-primary mb-3 mr-3 float-right">Participer</button>
-            </div>
+              class="btn btn-outline-primary mb-3 mr-3 float-right">Détails</button>
+            </a>
 
-            <div class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
-              <img
-              src="../img/imgConf.jpg"
-              alt="img-conf"
-              class="img-conf-profil mb-3">
+            <a href="page-conf.php" class="col-md-4 rounded m-4 shadow-sm h-25 p-0 pointer event">
+              <div class="position-relative w-100 mb-3">
+                <img
+                src="../img/imgConf.jpg"
+                alt="img-conf"
+                class="img-conf-profil">
+                <span class="conf-categ bg-info text-light p-1">[Formation]</span>
+              </div>
               <span class="text-info font-weight-light ml-3">mer. 21 nov, 9h45</span>
               <h5 class="p-3 mb-0">Nouveautés Angular 8+</h5>
               <span class="d-block pl-3 pr-3 mb-2">
@@ -108,11 +132,15 @@
                  <i class="p48 text-muted material-icons">location_on</i>
                  <span class="font-weight-light align-self-center">21 Rue Claude Tillier - 75012 Paris</span>
                </div>
+              <div class="float-left pl-3 pt-2">
+                <i class="text-muted material-icons dp48">person</i>
+                <span class="vertical-super">87</span>
+              </div>
               <button
               type="button"
               name="participer"
-              class="btn btn-outline-primary mb-3 mr-3 float-right">Participer</button>
-            </div>
+              class="btn btn-outline-primary mb-3 mr-3 float-right">Détails</button>
+            </a>
           </div>
         </div>
 
@@ -121,7 +149,7 @@
           <ul class="list-group ml-2 mt-4 shadow-sm">
             <li class="list-group-item pointer onCategory">Famille</li>
             <li class="list-group-item pointer onCategory">Formation</li>
-            <li class="list-group-item pointer onCategory">Nature et Aventure</li>
+            <li class="list-group-item pointer onCategory">Nature & Aventure</li>
             <li class="list-group-item pointer onCategory">Technologie</li>
           </ul>
         </div>
--- src/menu.php
+++ src/menu.php
@@ -1,24 +1,47 @@
-<nav class="navbar navbar-expand p-4 fixed-top bg-dark shadow">
+<nav class="navbar navbar-expand-xl p-4 fixed-top bg-dark shadow">
   <h2>
     <a href="./index.php" class="font-weight-light title-style">La Conférence</a>
   </h2>
+  <button
+  class="navbar-toggler"
+  type="button"
+  data-toggle="collapse"
+  data-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent"
+  aria-expanded="false"
+  aria-label="Toggle navigation">
+    <i
+    class="material-icons dp48 pointer p-2 rounded"
+    style="color: #fedc72;">reorder</i>
+  </button>
 
-  <ul class="navbar-nav ml-auto">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
     <li class="nav-item">
       <a
-      class="nav-link text-light font-weight-light pointer"
+      class="nav-link text-light font-weight-light pointer ml-4 mr-4 letter-space"
       href="./creation-conf.php"
       >Créer une conférence<a/>
     </li>
+    <form class="form-inline my-2 my-lg-0">
+      <input
+      class="form-control mr-sm-2"
+      style="width: 30rem;"
+      type="search"
+      placeholder="Rechercher…"
+      aria-label="Rechercher">
+    </form>
+  </ul>
+  <ul class="navbar-nav ml-auto">
     <li class="nav-item">
       <a
-      class="nav-link text-light font-weight-light pointer"
+      class="nav-link text-light font-weight-light pointer letter-space"
       href="./page-connexion.php"
       >Se connecter<a/>
     </li>
     <li class="nav-item">
       <a
-      class="nav-link text-light font-weight-light pointer"
+      class="nav-link text-light font-weight-light pointer letter-space"
       href="./page-inscription.php"
       >S'inscrire<a/>
     </li>
