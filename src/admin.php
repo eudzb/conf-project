4f12ed7bdcaea21b4190df433dc7321986c39f94
sc78200 <43121353+sc78200@users.noreply.github.com>
2019-06-27 17:48:48 +0200

    Add files via upload

--- src/admin.php
+++ src/admin.php
@@ -1,100 +1,58 @@
+<?php
+	include "code.php";
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 	<head>
     <meta charset="utf-8">
-    <title>Conferank</title>
+    <title>La Conférence</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
-  
 </head> 
 <body>
-<?php include 'menu.php';?>
+ <?php include 'menu.php';?> 
     <div class="content container up-space">
     	<h1 class="mb-5 text-center">ADMINISTRATEUR</h1>
       <h3 class="mb-3">Utilisateurs</h3>
 	  <table class="table table-striped mb-5">
 	    <thead>
 	      <tr>
+	        <th>Icon</th>
 	        <th>Nom</th>
 	        <th>Prénom</th>
 	        <th>Email</th>
+	        <th>Pseudo</th>	        
 	        <th>Admin</th>
 	        <th>Action</th>
 	      </tr>
 	    </thead>
-	    <tbody>
+	    <tbody><?php $i = 0;
+	      	 foreach ($participant as $key ) {
+	      		$a = $participant[$i]['img'];
+           		$src = 'data: image.jpeg;base64,'. $a;
+	      	 ?>
 	      <tr>
-	        <td>John</td>
-	        <td>Doe</td>
-	        <td>john@example.com</td>
-	        <td>0</td>
-	        <td><button>Supprimer</button></td>
+	        <td>
+	        	<?php echo '<img src="'.$src.'" class="rounded-circle pointer p-1" 
+	        			alt="participant-icon" style="height:80px; width:80px">'; 
+	        	?>
+	        </td>
+	        <td class="align-middle"><?php echo $participant[$i]['lastName'] ?></td>
+	        <td class="align-middle"><?php echo $participant[$i]['firstName'] ?></td>
+	        <td class="align-middle"><?php echo $participant[$i]['email'] ?></td>
+	        <td class="align-middle"><?php echo $participant[$i]['pseudo'] ?></td>
+	        <td class="align-middle">0</td>
+	        <td class="align-middle">
+	        		<a href="delete.php?id=<?php echo  $participant[$i]['id']?>" class="btn btn-danger" onClick="return checkMe(), alert('Utilisateur Supprimé !')">Supprimer
+	        		</a>
+	        </td>
 	      </tr>
-	      <tr>
-	        <td>Mary</td>
-	        <td>Moe</td>
-	        <td>mary@example.com</td>
-	        <td>0</td>
-	        <td><button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>July</td>
-	        <td>Dooley</td>
-	        <td>july@example.com</td>
-	        <td>1</td>
-	        <td><button>Supprimer</button></td>
-	      </tr>
-	   </tbody>
-	  </table>
-
-	  <h3 class="mb-3">Categories</h3>
-	  <table class="table table-striped mb-5">
-	    <tbody>
-	    <thead>
-	      <tr>
-	        <th>Nom</th>
-	        <th>Actions</th>
-	      </tr>
-	    </thead>
-	      <tr>
-	        <td>Famille</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Formation</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Nature et Aventure</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Photographie</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Cuisine et Boissons</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Musique</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Danse</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Sport et Fitness</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
-	      <tr>
-	        <td>Croyance</td>
-	        <td><button>Editer</button> <button>Supprimer</button></td>
-	      </tr>
+	     <?php $i++ ; }?>
 	   </tbody>
 	  </table>
 
@@ -103,33 +61,30 @@
 	    <thead>
 	      <tr>
 	        <th>Titres</th>
-	        <th>Organisateur</th>
-	        <th>Categories</th>
-	        <th>Participants</th>
+	        <th>Organisateur</th> 
 	        <th>Date</th>
 	        <th>Heure</th>
 	        <th>Descriptions</th>
-	        <th>Notes</th>
+	        <th>Adresse</th>
+	        <th>Ville</th>
 	        <th>Actions</th>
 	      </tr>
 	    </thead>
 	    <tbody>
+	    	<?php $i = 0;
+	      	 foreach ($row as $key ) {	      		
+	      	 ?>
 	      <tr>
-	        <td>PHP</td>
-	        <td>John</td>
-	        <td>Technologie</td>
-	        <td>15</td>
-	        <td>27/06/2019</td>
-	        <td>14h00 à 15h00</td>
-	        <td>PHP est un langage de développement orienté Web, lors de cette conférence je vous prensenterai les fonctionnalité disponible.
-			étape 1 : L'histoire de PHP
-			étape 2 : Ses qualité/défaut 
-			étape 3 : Les framework PHP
-			</td>
-			<td>4.5</td>
-	        <td><button>Editer</button> <button class="mt-2">Supprimer</button></td>
-	      </tr>
-	      
+	        <td class="align-middle"><?php echo $row[$i]['title'] ?></td>
+	        <td class="align-middle"><?php echo $row[$i]['firstName'] ?></td>
+	        <td class="align-middle"><?php echo $row[$i]['la_date_a_afficher'] ?></td>
+	        <td class="align-middle"><?php echo "De ".$dateStart." à ".$dateEnd ?></td>
+	        <td class="align-middle"><?php echo $row[$i]['description'] ?></td>
+	        <td class="align-middle"><?php echo $row[$i]['adress'] ?></td>
+	        <td class="align-middle"><?php echo $row[$i]['city'] ?></td>
+	        <td class="align-middle"><a href="" class="btn btn-danger">Supprimer</a></td>
+	      </tr> 
+	       <?php $i++ ; }?>
 	   </tbody>
 	  </table>
 	</div>
---
+++ src/code.php
@@ -0,0 +1,66 @@
+<?php
+//function pageConference ($idConf){
+include"connexionbdd.php";
+$idConf = 5;
+// Titre, description, date, heure de début/fin, adresse, code postale, ville, nom de l'intervenant
+$conferenceInfos = $bdd->query("
+  SELECT conference.id, conference.title, conference.description, date_format(dateStart, '%d-%m-%Y') as la_date_a_afficher ,
+  conference.dateStart, conference.dateEnd, conference.adress, conference.cp, conference.city, conference.logo,
+  user.firstName, user.img,
+  category.category_name as category
+  FROM user, conference, category
+  WHERE conference.user_id = user.id
+  AND conference.category_id = category.id");
+$row = $conferenceInfos->fetchAll();
+
+$titre = $row[0]['title'];
+$organisateur = $row[0]['user.firstName'];
+$date = $row[0]['date_format'];
+$Heure = $row[0]['dateStart'];
+$description = $row[0]['description'];
+$adress = $row[0]['adress'];
+$city = $row[0]['city'];
+
+//echo $row[0]['category'];
+$imgConf = 'data: image.jpeg;base64,'. $row[0]['logo'];
+$imgPseudo = 'data: image.jpeg;base64,'. $row[0]['img'];
+//print_r($row);
+$participantInfos = $bdd->query("
+  SELECT COUNT(id_conference) as attendees
+  FROM attendees
+  WHERE id_conference =$idConf");
+$nbParticipant = $participantInfos->fetchAll();
+$conferenceInfos = $bdd->query("
+  SELECT * 
+  FROM user");
+$participant = $conferenceInfos->fetchAll();
+
+$prenom = $participant[0]['firstName'];
+$id = $participant[0]['id'];
+$pseudo = $participant[0]['pseudo'];
+$nom = $participant[0]['lastName'];
+$email = $participant[0]['email'];
+
+//DATE
+$dateFirst = date('G:i', strtotime($row[0]['dateStart']));
+$dateStart = str_replace(":", "h", $dateFirst);
+$dateSecond = date('G:i', strtotime($row[0]['dateEnd']));
+$dateEnd = str_replace(":", "h", $dateSecond);
+
+
+$date = new DateTime($row[0]['la_date_a_afficher']);
+$nom_jour_fr = array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+$mois_fr = Array("", "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août",
+        "septembre", "octobre", "novembre", "décembre");
+list($nom_jour, $jour, $mois, $annee) = explode('-', $date->format('w-d-n-Y'));
+
+//Categories
+$category = $bdd->query("SELECT * FROM `category` ORDER BY category_name");
+$categoryConf = $category->fetchAll();
+
+$nom_de_categorie = $categoryConf[0]['category_name'];
+
+//Delete
+
+
+
